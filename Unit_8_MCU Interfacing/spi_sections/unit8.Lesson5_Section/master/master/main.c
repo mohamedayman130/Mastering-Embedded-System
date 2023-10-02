0560e5a0ca754c6d403f030328ede20e5c572169
mohamed ayman <117905345+mohamedayman130@users.noreply.github.com>
2023-10-02 13:05:56 +0300

    Add files via upload

---
+++ Unit_8_MCU Interfacing/spi_sections/unit8.Lesson5_Section/master/master/main.c
@@ -0,0 +1,40 @@
+/*
+ * 15963.c
+ *
+ * Created: 01/10/2023 23:18:25
+ * Author : Alfanan02
+ */ 
+
+#include <avr/io.h>
+#include <stdint.h>
+#include <avr/delay.h>
+#include "hal/lcd.h"
+void spi_init()
+{
+	DDRB |= 1<<4 | 1<<5 |1<<7;
+	SPCR|=1<<6 | 1<<4 ;
+}
+
+uint8_t send_rec_data(uint8_t data)
+{
+	SPDR = data;
+	while (! (SPSR & 1<<7));
+	return SPDR;
+}
+
+int main(void)
+{	
+	spi_init();
+	LCD_INIT();
+	uint8_t x='z';
+	LCD_WRITE_STRING("master");
+	
+	while (1) 
+    {
+	LCD_WRITE_COMMAND(0xc0);
+	LCD_WRITE_CHAR(send_rec_data(x--));
+	if(x=='a'-1) x='z';
+	_delay_ms(7000);
+    }
+}
+
---
+++ Unit_8_MCU Interfacing/spi_sections/unit8.Lesson5_Section/slave/slave/main.c
@@ -0,0 +1,32 @@
+
+#include <avr/io.h>
+#include <stdint.h>
+#include "hal/lcd.h"
+void spi_init()
+{
+	DDRB|=1<<6;
+	SPCR|=1<<6;
+}
+
+uint8_t send_rec_data(uint8_t data)
+{   
+	SPDR = data;
+	while (! (SPSR & 1<<7));
+	return SPDR;
+}
+int main(void)
+{
+	spi_init();
+	LCD_INIT();
+	uint8_t x='a';
+	LCD_WRITE_STRING("slave:");
+	
+    while (1) 
+    {
+		LCD_WRITE_COMMAND(0xc0);
+		LCD_WRITE_CHAR(send_rec_data(x++));
+		if(x=='z'+1) x='a';
+		_delay_ms(7000);
+    }
+}
+
