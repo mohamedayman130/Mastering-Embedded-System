6645b5c5932fa6b26a23937d474123d4b6f0efb2
mohamed ayman <117905345+mohamedayman130@users.noreply.github.com>
2024-03-02 00:55:45 +0200

    Delete ARM/states and modes/arm directory

--- ARM/states and modes/arm/modes_and_states_trial/Debug/Src/rcc.d
+++
@@ -1,8 +0,0 @@
-Src/rcc.o: ../Src/rcc.c ../Src/inc/rcc.h ../Src/inc/stm32f103x6.h \
- ../Src/inc/stm32f103x6_gpio_driver.h
-
-../Src/inc/rcc.h:
-
-../Src/inc/stm32f103x6.h:
-
-../Src/inc/stm32f103x6_gpio_driver.h:
