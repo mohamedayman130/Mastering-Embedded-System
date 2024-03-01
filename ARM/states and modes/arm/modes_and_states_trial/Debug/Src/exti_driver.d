1b69f710e61a9d755cfac3a9eb279f533833f052
mohamed ayman <117905345+mohamedayman130@users.noreply.github.com>
2024-03-02 01:02:55 +0200

    Delete ARM/states and modes/arm directory

--- ARM/states and modes/arm/modes_and_states_trial/Debug/Src/exti_driver.d
+++
@@ -1,8 +0,0 @@
-Src/exti_driver.o: ../Src/exti_driver.c ../Src/inc/EXTI_dRIVER.h \
- ../Src/inc/stm32f103x6.h ../Src/inc/stm32f103x6_gpio_driver.h
-
-../Src/inc/EXTI_dRIVER.h:
-
-../Src/inc/stm32f103x6.h:
-
-../Src/inc/stm32f103x6_gpio_driver.h:
